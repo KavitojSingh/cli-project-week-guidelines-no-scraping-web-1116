669061b912c2fdfc540d60d7a236063114eaa2b9
github username <github email address>
2016-12-06 16:40:32 -0500

    more

--- lib/models/water_consumption.rb
+++ lib/models/water_consumption.rb
@@ -8,7 +8,31 @@ class WaterConsumption
 	end
 
 	def central_park_reservoir
-		(((@value.to_i)*1000000)/1000000000.0).round(1)
+		((((@value.to_i)*1000000)/1000000000.0)*100).round(1)
+	end
+
+	def lake_erie
+		sprintf('%.15f', ((((@value.to_f)*1000000)/1277295890000000000000000.0)*100))
+	end
+
+	def bottles_of_water
+		(((@value.to_i)*1000000)*8)
+	end
+
+	def washing_machines
+		(((@value.to_i)*1000000)/27)
+	end
+
+	def toilets
+		(((@value.to_i)*1000000)/2)
+	end
+
+	def olympic_pools
+		(((@value.to_i)*1000000)/660430)
+	end
+
+	def human_mouth_fulls
+		(((@value.to_i)*1000000)*32)
 	end
 
 end
--- lib/runners/water_consumption_cli.rb
+++ lib/runners/water_consumption_cli.rb
@@ -1,5 +1,6 @@
 class WaterConsumptionCLI
 
+
 	def call
 		puts "Calling the Application!"
 
@@ -11,17 +12,48 @@ class WaterConsumptionCLI
 		elsif input == "exit"
 			exit
 		else
-			water_data = WaterConsumptionAPICaller.new(input)
-			variable = water_data.fetch_water_consumption.central_park_reservoir
-			puts "In #{input}, New York City consumed water equivalent to #{variable} Central Park Reservoirs!!!"
+			methods = ["central_park_reservoir", "lake_erie", "bottles_of_water", "washing_machines", "toilets", "olympic_pools", "human_mouth_fulls"]
+			chosen_method = methods.sample
+			if chosen_method == "central_park_reservoir"
+				water_data = WaterConsumptionAPICaller.new(input)
+				variable = water_data.fetch_water_consumption.send("central_park_reservoir")
+				puts "In #{input}, New York City consumed water equivalent to #{variable}% of Central Park Reservoir!"
+			elsif chosen_method == "lake_erie"
+				water_data = WaterConsumptionAPICaller.new(input)
+				variable = water_data.fetch_water_consumption.send("lake_erie")
+				puts "In #{input}, New York City consumed water equivalent to #{variable}% of Lake Erie!"
+			elsif chosen_method == "bottles_of_water"
+				water_data = WaterConsumptionAPICaller.new(input)
+				variable = water_data.fetch_water_consumption.send("bottles_of_water")
+				puts "In #{input}, New York City consumed water equivalent to #{variable} bottles of water!"
+			elsif chosen_method == "washing_machines"
+				water_data = WaterConsumptionAPICaller.new(input)
+				variable = water_data.fetch_water_consumption.send("washing_machines")
+				puts "In #{input}, New York City consumed water equivalent to #{variable} loads of laundry in a standard washing machine!"
+			elsif chosen_method == "toilets"
+				water_data = WaterConsumptionAPICaller.new(input)
+				variable = water_data.fetch_water_consumption.send("toilets")
+				puts "In #{input}, New York City consumed water equivalent to #{variable} toilet flushes!"
+			elsif chosen_method == "olympic_pools"
+				water_data = WaterConsumptionAPICaller.new(input)
+				variable = water_data.fetch_water_consumption.send("olympic_pools")
+				puts "In #{input}, New York City consumed water equivalent to #{variable} Olympic sized swimming pools!"	
+			elsif chosen_method == "human_mouth_fulls"
+				water_data = WaterConsumptionAPICaller.new(input)
+				variable = water_data.fetch_water_consumption.send("human_mouth_fulls")
+				puts "In #{input}, New York City consumed water equivalent to #{variable} average human mouth fulls!"			
+			else 
+				puts "Woohoo!"
+			end
 		end
 
 		# water_data = WaterConsumptionAPICaller.new(input)
 		# variable = water_data.fetch_water_consumption.central_park_reservoir
 
 		# puts "In #{input}, New York City consumed water equivalent to #{variable} Central Park Reservoirs!!!"
-
 	end
+
+
 
 	def help
 		puts "Type 'exit' to exit the program"
@@ -30,4 +62,6 @@ class WaterConsumptionCLI
 
 
 end
+
+
 
