280a502d47ab5702d03cda66b6713c918509a2c1
github username <github email address>
2016-12-06 17:04:01 -0500

    we got really far

--- lib/runners/water_consumption_cli.rb
+++ lib/runners/water_consumption_cli.rb
@@ -3,9 +3,22 @@ class WaterConsumptionCLI
 
 	def call
 		puts "Welcome Guest! We are aobut to make a splash with some fun facts about New York City's water usage!"
+		run 
+	end
+
+
+	def help
+		puts "Type 'exit' to exit the program"
+		puts "Type 'help' to view this menu again"
+	end
+
+
+
+	def run
 
 		print "Please enter a year between 1979 and 2009: "
 		input = gets.strip
+
 
 		if input == "help"
 			help
@@ -43,25 +56,11 @@ class WaterConsumptionCLI
 				variable = water_data.fetch_water_consumption.send("human_mouth_fulls")
 				puts "In #{input}, New York City consumed water equivalent to #{variable} average human mouth fulls!"			
 			else 
-				puts "Woohoo!"
+				puts "If you're seeing this, we did something very, very wrong . . . "
 			end
 		end
-
-		# water_data = WaterConsumptionAPICaller.new(input)
-		# variable = water_data.fetch_water_consumption.central_park_reservoir
-
-		# puts "In #{input}, New York City consumed water equivalent to #{variable} Central Park Reservoirs!!!"
-	end
-
-
-
-	def help
-		puts "Type 'exit' to exit the program"
-		puts "Type 'help' to view this menu again"
-	end
+		run
+	end 
 
 
 end
-
-
-
